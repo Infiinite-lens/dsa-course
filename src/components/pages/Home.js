c8de4ab7b42f7dc5d89a37a3fe5660bbb671cde7
lens <okorelens27@gmail.com>
2022-06-01 17:13:09 +0300

    Updated {home} background color, fonts and color

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,13 +4,15 @@ import classes from "./Home.module.css";
 function Home() {
   return (
     <div className={classes["hero-page"]}>
-      <div className={classes.text}>
-        <h1>Data Structures</h1>
-        <div className={classes.and}>and</div>
-        <h1>Algorithms</h1>
-        <div className={classes.and}>by</div>
-        <div className={classes.name}>
-          <h2>Lens William</h2>
+      <div className={classes["hero-wrapper"]}>
+        <div className={classes.text}>
+          <h1>Data Structures</h1>
+          <div className={classes.and}>and</div>
+          <h1>Algorithms</h1>
+          <div className={classes.and}>by</div>
+          <div className={classes.name}>
+            <h2>Lens William</h2>
+          </div>
         </div>
       </div>
     </div>
