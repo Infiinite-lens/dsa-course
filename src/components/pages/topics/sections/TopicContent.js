cf4e052a0ec2f6a279784cba5032cfcda98e5a6a
lens <okorelens27@gmail.com>
2022-06-02 04:25:52 +0300

    Added styling

--- src/components/pages/topics/sections/TopicContent.js
+++ src/components/pages/topics/sections/TopicContent.js
@@ -3,7 +3,34 @@ import React from "react";
 import classes from "./TopicContent.module.css";
 
 function TopicContent() {
-  return <div className={classes["topic-content"]}>TopicContent</div>;
+  return (
+    <div className={classes["topic-content"]}>
+      <h1>Topic Title</h1>
+      <div className={classes.wrapper}>
+        <p>
+          Introductory Message....
+          <br /> Introductory message
+          .............######################################
+          ######################################################
+          ############################
+          <br />
+          ............. ######################################
+          ######################################################
+          ############################
+          <br />
+          ............. ######################################
+          ######################################################
+          ############################
+          <br />
+          ............. ######################################
+          ######################################################
+          ############################
+        </p>
+        <div>Code Snippet and Pseudocode</div>
+        <div>Code explanation and Big'O' Table</div>
+      </div>
+    </div>
+  );
 }
 
 export default TopicContent;
